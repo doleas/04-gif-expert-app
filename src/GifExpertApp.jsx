d72e162338ba972cd0937b4e7efb3b0c3562c7fb
doleas <david.oleas@iess.gob.ec>
2022-11-11 16:54:02 -0500

    hooks

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+    //para generar un listado
+    const [categories, setCategories] = useState([ 'One Punch' ]);
+
+    console.log(categories);
+
     return (
         <>
+        {/* Esto va  a estar dividido en tres partes */}
+            {/* titulo */}
             <h1> GifExpertApp </h1>
+            {/* input */}
+
+            {/* listado de gif */}
+            <ol>
+                <li>ABC</li>
+                <li>123</li>
+                <li>.</li>
+            </ol>
+                {/* gif item */}
         </>
     )
 } 
